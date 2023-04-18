720786d2aeeaed079f0ec48c353eb9619a7f4db7
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-04-18 18:13:34 +0200

    clean

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,42 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App';
+import Layout from './layouts/layout';
+import Home from './pages/home';
+import NoMatch from './pages/no-match';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'about',
+        async lazy() {
+          const { loader, default: About } = await import('./pages/about');
+
+          return {
+            loader,
+            Component: About
+          };
+        }
+      },
+      {
+        path: '*',
+        element: <NoMatch />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
   </React.StrictMode>
 );
