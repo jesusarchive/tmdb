73a15675c6b9a1ed89169bc17b9f2297fc26f391
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-04-21 00:19:35 +0200

    add home view tests

---
+++ src/pages/home/__tests__/home.test.tsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/named
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { describe, expect, test } from 'vitest';
+
+import Home from '../home';
+
+describe('<Home />', () => {
+  test('Home mounts properly', () => {
+    const wrapper = render(<Home />);
+    expect(wrapper).toBeTruthy();
+
+    // Get by element
+    const h1 = wrapper.container.querySelector('h1');
+    expect(h1?.textContent).toBe('TMDb');
+
+    // Get by text using the React testing library
+    const text = screen.getByText(/TMDb/i);
+    expect(text.textContent).toBeTruthy();
+  });
+});
