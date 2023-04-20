aacf575e1d321ca5e02573c2610197929eb6e1c8
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-04-20 14:26:32 +0200

    styles wp

--- src/pages/no-match/no-match.tsx
+++ src/pages/no-match/no-match.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NoMatch = () => {
   return (
-    <div>
+    <div className="min-h-[81vh] container mx-auto">
       <h2>Nothing to see here!</h2>
       <p>
         <Link to="/">Go to the home page</Link>
