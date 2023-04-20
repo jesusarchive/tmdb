fe47590419c0d37acfef4b36279a192d81312b6c
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-04-20 14:08:16 +0200

    show 100 titles merging the results of several pages in popular movies view

--- src/pages/chart/chart.tsx
+++ src/pages/chart/chart.tsx
@@ -3,9 +3,10 @@ import { StarIcon } from '@heroicons/react/24/solid';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Button, Pagination, Table } from '../../components';
+import { Button, Divider, Pagination, Table } from '../../components';
 import { IMAGE_BASE_URL } from '../../services/helpers/constants';
 import { getPopularMovies, MovieType } from '../../services/movie';
+import { uniq } from '../../utils';
 
 /**
  *
@@ -18,44 +19,63 @@ import { getPopularMovies, MovieType } from '../../services/movie';
  */
 const Chart = () => {
   const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
-  const [maxPage, setMaxPage] = useState(-1);
+  // const [page, setPage] = useState(1);
+  // const [maxPage, setMaxPage] = useState(-1);
   const [movies, setMovies] = useState([] as Array<MovieType>);
 
   useEffect(() => {
     (async () => {
       setLoading(true);
-      const data = await getPopularMovies(page);
+      const data = await getPopularMovies(1);
+      const firstPageMovies = data.results;
       console.log(data);
-      setMaxPage(data.total_pages);
-      setMovies(data.results);
+      // setMaxPage(data.total_pages);
+      // setMovies(data.results);
+      if (data.total_pages > 6) {
+        const nextPagesData = await Promise.all([2, 3, 4, 5, 6].map(async (el) => await getPopularMovies(el)));
+        const newMovies = nextPagesData.reduce((acc, el) => acc.concat(el.results), firstPageMovies);
+        console.log(uniq(newMovies).slice(0, 100));
+        setMovies(uniq(newMovies).slice(0, 100));
+      }
       setLoading(false);
     })();
-  }, [page]);
+  }, []);
 
-  const handlePreviousPaginationClick = () => {
-    const newPage = page - 1;
-    if (newPage > 0) {
-      setPage(newPage);
-    }
-  };
+  // useEffect(() => {
+  //   (async () => {
+  //     setLoading(true);
+  //     const data = await getPopularMovies(page);
+  //     console.log(data);
+  //     setMaxPage(data.total_pages);
+  //     setMovies(data.results);
+  //     setLoading(false);
+  //   })();
+  // }, [page]);
 
-  const handleNextPaginationClick = () => {
-    if (maxPage < 0) {
-      return;
-    }
+  // const handlePreviousPaginationClick = () => {
+  //   const newPage = page - 1;
+  //   if (newPage > 0) {
+  //     setPage(newPage);
+  //   }
+  // };
 
-    const newPage = page + 1;
-    if (newPage <= maxPage) {
-      setPage(newPage);
-    }
-  };
+  // const handleNextPaginationClick = () => {
+  //   if (maxPage < 0) {
+  //     return;
+  //   }
+
+  //   const newPage = page + 1;
+  //   if (newPage <= maxPage) {
+  //     setPage(newPage);
+  //   }
+  // };
 
   return (
     <div className="min-h-[80vh] w-full flex p-5">
       <div className="container mx-auto flex flex-col justify-around">
         <div className="h-full w-full flex flex-col justify-around">
-          <div className="pb-5">
+          {/* HEADER */}
+          <div className="">
             <h3 className="text-xl pb-2">TMDb Charts</h3>
             {/* CHART TITLE */}
             <h1 className="text-3xl">Most Popular movies</h1>
@@ -63,73 +83,85 @@ const Chart = () => {
             <p>As determined by TMDb Users</p>
           </div>
 
+          <Divider />
+
           {loading ? (
             <div className="h-full w-full flex">
               <span>loading...</span>
             </div>
-          ) : (
-            <Table className="min-h-[65vh] w-full">
-              <Table.Head>
-                <span />
-                <span>Title</span>
-                <span>TMDb Rating</span>
-                <span>Your Rating</span>
-                <span />
-              </Table.Head>
+          ) : movies?.length ? (
+            <div>
+              <div className="pt-5 pb-5">
+                <p className="text-base">{`Showing ${movies.length} Titles`}</p>
+              </div>
+              {/* RESULTS TABLE */}
+              <Table className="min-h-[65vh] w-full" compact>
+                <Table.Head>
+                  <span />
+                  <span>Title</span>
+                  <span>TMDb Rating</span>
+                  <span>Your Rating</span>
+                  <span />
+                </Table.Head>
 
-              <Table.Body>
-                {movies?.map((movie) => (
-                  <Table.Row key={movie.id}>
-                    {/* POSTER */}
-                    <div>
-                      <Link to={`/title/${movie.id}`}>
-                        <img className="w-14" src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt="poster" />
-                      </Link>
-                    </div>
-                    {/* TITLE */}
-                    <div className="space-x-2">
-                      <Link className="link-info" to={`/title/${movie.id}`}>
-                        {movie.title}
-                      </Link>
-                      <span>{`(${new Date(movie.release_date).getFullYear()})`}</span>
-                    </div>
-                    {/* RATING */}
-                    <div className="flex space-x-1">
-                      <StarIcon className="h-6 w-6 text-yellow-600" />
-                      <span className="font-bold">{movie.vote_average.toFixed(1)}</span>
-                    </div>
-                    {/* USER RATING */}
-                    <div className="flex space-x-1">
-                      <StarIconOutline className="h-6 w-6" />
-                    </div>
-                    {/* BOOKMARK */}
-                    <div>
-                      <svg
-                        className="cursor-pointer"
-                        width="30px"
-                        height="30px"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="black"
-                        stroke="white"
-                        strokeWidth="1"
-                        strokeLinecap="round"
-                        strokeLinejoin="miter"
-                      >
-                        <polygon points="20 22 12 16 4 22 4 2 20 2 20 22"></polygon>
-                        <line x1="12" y1="6" x2="12" y2="12"></line>
-                        <line x1="15" y1="9" x2="9" y2="9"></line>
-                      </svg>
-                    </div>
-                  </Table.Row>
-                ))}
-              </Table.Body>
-            </Table>
+                <Table.Body>
+                  {movies?.map((movie) => (
+                    <Table.Row key={movie.id}>
+                      {/* POSTER */}
+                      <div className="w-12">
+                        <Link to={`/title/${movie.id}`}>
+                          <img className="w-full" src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt="poster" />
+                        </Link>
+                      </div>
+                      {/* TITLE & YEAR */}
+                      <div className="space-x-2 text-lg">
+                        <Link className="link" to={`/title/${movie.id}`}>
+                          {movie.title}
+                        </Link>
+                        <span>{`(${new Date(movie.release_date).getFullYear()})`}</span>
+                      </div>
+                      {/* RATING */}
+                      <div className="flex space-x-1 text-lg">
+                        <StarIcon className="h-6 w-6 text-yellow-600" />
+                        <span className="font-bold">{movie.vote_average.toFixed(1)}</span>
+                      </div>
+                      {/* USER RATING */}
+                      <div className="flex space-x-1">
+                        <StarIconOutline className="h-6 w-6" />
+                      </div>
+                      {/* BOOKMARK */}
+                      <div>
+                        <svg
+                          className="cursor-pointer"
+                          width="30px"
+                          height="30px"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="black"
+                          stroke="white"
+                          strokeWidth="1"
+                          strokeLinecap="round"
+                          strokeLinejoin="miter"
+                        >
+                          <polygon points="20 22 12 16 4 22 4 2 20 2 20 22"></polygon>
+                          <line x1="12" y1="6" x2="12" y2="12"></line>
+                          <line x1="15" y1="9" x2="9" y2="9"></line>
+                        </svg>
+                      </div>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+            </div>
+          ) : (
+            <div className="min-h-[65vh]">
+              <span>No results</span>
+            </div>
           )}
         </div>
 
         {/* PAGINATION */}
-        <Pagination className="pt-20 self-center">
+        {/* <Pagination className="pt-20 self-center">
           <Button className="btn" onClick={handlePreviousPaginationClick}>
             «
           </Button>
@@ -140,7 +172,7 @@ const Chart = () => {
           <Button className="btn" onClick={handleNextPaginationClick}>
             »
           </Button>
-        </Pagination>
+        </Pagination> */}
       </div>
     </div>
   );
