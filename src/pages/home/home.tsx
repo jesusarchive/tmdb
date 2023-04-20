4a9a1e7a3d227bdbe20c17b9cbc1bd84bfff502b
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-04-20 14:03:05 +0200

    update home styles

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,7 @@ import HomeCinema from '../../assets/home-cinema.svg';
 const Home = () => {
   return (
     <div className="h-full w-full container mx-auto flex justify-center items-center">
-      <img className="pt-60" src={HomeCinema} alt="Home cinema" />
+      <img className="h-[40vh]" src={HomeCinema} alt="Home cinema" />
     </div>
   );
 };
