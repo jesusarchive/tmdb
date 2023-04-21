970795094618fc810b5e09a0fdd9e6762759e62b
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-04-21 22:22:18 +0200

    add rating update when user ratings view is loaded

--- src/pages/user-ratings/user-ratings.tsx
+++ src/pages/user-ratings/user-ratings.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { getGuestSessionRatedMovies } from '../../services/movie';
+import { updateGuestSessionRatedMovies } from '../../store/actions';
 import { useStore } from '../../store/store';
 import RatingList from './rating-list';
 
 const UserRatings = () => {
-  const { state } = useStore();
+  const { state, dispatch } = useStore();
+  const [loading, setLoading] = useState(false);
+
+  // TODO: Review if needed, check to trigger the update on rate click
+  const updateUserRatedMovies = async () => {
+    const isGuestSessionActive = !!state?.guest?.guest_session_id;
+    if (isGuestSessionActive) {
+      const guestSessionRatedMovies = await getGuestSessionRatedMovies(state.guest.guest_session_id);
+      dispatch(updateGuestSessionRatedMovies(guestSessionRatedMovies));
+
+      return guestSessionRatedMovies;
+    }
+  };
+
+  const init = async () => {
+    setLoading(true);
+    await updateUserRatedMovies();
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    (async () => {
+      init();
+    })();
+  }, []);
 
   return (
     <div className="min-h-[80vh] container mx-auto">
-      {state?.guest?.rated_movies?.results?.length > 0 ? (
+      {loading ? (
+        <span>loading...</span>
+      ) : state?.guest?.rated_movies?.results?.length > 0 ? (
         <div className="">
           <h1 className="text-3xl pb-5">Your Ratings</h1>
 
@@ -19,7 +47,7 @@ const UserRatings = () => {
           )}
         </div>
       ) : (
-        <span className="text-3xl">No rated movies</span>
+        <span className="">No rated movies</span>
       )}
     </div>
   );
