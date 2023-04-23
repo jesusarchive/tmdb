a3e217d50b4e82dd23a857b67edd13a778a80d1a
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-04-23 13:49:23 +0200

    update auth service

--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,10 +1,12 @@
 import { API_KEY, BASE_URL } from '../../config/api';
 
-export const getGuestSession = async (): Promise<{
+export type GuestSessionType = {
   success: boolean;
   guest_session_id: string;
   expires_at: string;
-}> => {
+};
+
+export const getGuestSession = async (): Promise<GuestSessionType> => {
   const response = await fetch(`${BASE_URL}/3/authentication/guest_session/new?api_key=${API_KEY}`);
   const data = await response.json();
 
